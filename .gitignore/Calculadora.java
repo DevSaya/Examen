1004e39d3734b24bb57de7f3a25ef674f23b9160
DevSaya <34213633+DevSaya@users.noreply.github.com>
2018-04-12 16:40:11 +0200

    Add files via upload

---
+++ .gitignore/Calculadora.java
@@ -0,0 +1,20 @@
+package Examen_UF2;
+
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Calculadora {
+
+	@Test
+	public void testresidu() {
+		assertTrue(CalculadoraTest.residu(4));
+
+	}
+	
+	
+
+
+}
